6edee7eca1515b784fdd342f85323983a9e1b126
Dmytro Dzhunenko <ddzhunenko@gmail.com>
2019-08-29 14:51:25 +0300

    fixed bugs

--- src/main/java/edu/cursor/service/LibraryActions.java
+++ src/main/java/edu/cursor/service/LibraryActions.java
@@ -9,10 +9,12 @@ import java.util.List;
 public interface LibraryActions {
 
     List<Author> addAuthor (Author author);
+    List<Author> getAuthors();
+    List<Book> getBooks();
     void deleteBook(Book book);
     void refreshBook(int idOldBook, Book newBook);
     List<Author> addBook(Book book);
-    List<Book> sortBookByGenre(Genre genre);
+    List<Book> sortBookByGenre(String genre);
     List<Book> sortBookByAuthor(String authorLastName);
 
 }
